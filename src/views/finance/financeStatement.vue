89052205e82706b06597412da19cee4938f5a033
linsg <linsg@hspaces.cn>
2019-12-24 12:46:03 +0800

    update

--- src/views/finance/financeStatement.vue
+++ src/views/finance/financeStatement.vue
@@ -14,8 +14,8 @@
                       placeholder="结束日期时间"
                       value-format="yyyy-MM-dd">
       </el-date-picker>
-      <el-input placeholder="请输入用户名/电话"
-                v-model="listQuery.keyword"
+      <el-input placeholder="请输入学校名称"
+                v-model="listQuery.course_site"
                 style="width: 320px;"
                 clearable>
       </el-input>
@@ -32,7 +32,7 @@
                  plain
                  style="float:right;"
                  icon="el-icon-search"
-                 @click="handleFilter">搜索</el-button>
+                 @click="handleFilter(value)">搜索</el-button>
     </div>
     <el-table v-loading="listLoading"
               :data="list"
@@ -44,27 +44,27 @@
                        label="序号"
                        width="95">
         <template slot-scope="scope">
-          {{ scope.$index+1 }}
+          {{ scope.row.id }}
         </template>
       </el-table-column>
       <el-table-column align="center"
                        label="分点名称">
         <template slot-scope="scope">
-          {{ scope.row.avatar }}
+          {{ scope.row.course_site.name }}
         </template>
       </el-table-column>
       <el-table-column label="结算时间段"
                        width="110"
                        align="center">
         <template slot-scope="scope">
-          <span>{{ scope.row.nickname }}</span>
+          <span>{{ scope.row.start_date }} - {{scope.row.end_date}}</span>
         </template>
       </el-table-column>
       <el-table-column label="金额"
                        width="110"
                        align="center">
         <template slot-scope="scope">
-          {{ scope.row.tel }}
+          {{ scope.row.amount }}
         </template>
       </el-table-column>
       <el-table-column class-name="status-col"
@@ -72,7 +72,13 @@
                        width="110"
                        align="center">
         <template slot-scope="scope">
-          <span>{{scope.row.name}}</span>
+          <!-- <span>{{scope.row.status}}</span> -->
+          <div v-if="scope.row.status == '1' ">
+            已处理
+          </div>
+          <div v-else-if="scope.row.status == '2'">
+            未处理
+          </div>
         </template>
       </el-table-column>
       <el-table-column align="center"
@@ -81,7 +87,9 @@
         <template slot-scope="scope">
           <el-button size="mini"
                      type="danger"
-                     @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                     v-if="scope.row.status == '2' "
+                     @click="handle(scope.row)">处理</el-button>
+          <span v-else-if="scope.row.sale_status == '1' "></span>
         </template>
       </el-table-column>
     </el-table>
@@ -97,42 +105,92 @@
                      :total="total">
       </el-pagination>
     </div>
+    <el-dialog :visible.sync="dialogVisible"
+               :close-on-click-modal="false">
+      <el-form ref="form"
+               :model="form"
+               label-position="left"
+               label-width="130px">
+        <el-form-item label="请上传凭证">
+          <UploadList v-model="form.proof"></UploadList>
+        </el-form-item>
+
+      </el-form>
+      <div slot="footer"
+           class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary"
+                   @click="saveData"
+                   :loading="btnLoading">确定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import request from "@/utils/request";
-
+import UploadList from '@/components/UploadList'
 export default {
+  components: {
+    UploadList
+  },
   data () {
     return {
       options: [
         {
           value: '选项1',
-          label: '未选择',
+          label: '未处理',
         },
         {
           value: '选项2',
-          label: '已选择',
+          label: '已处理',
         },
       ],
-      value: '未选择',
+      dialogVisible: false,
+      value: '',
       list: null,
+      total: null,
+      btnLoading: false,
       listLoading: true,
       listQuery: {
         page: 1,
         limit: 10,
-        keyword: '',
         end_datetime: '',
         start_datetime: '',
+        course_site: '',
+        handle_status: '',
       },
+      form: {
+        id: '',
+        proof: [],
+      },
+      flag: 0
     }
   },
   created () {
-    // this.getList()
+    this.getList()
+  },
+  watch: {
+    dialogVisible (newVal, oldVal) {
+      // 编辑框一异隐藏，马上清除旧数据
+      if (newVal === false) {
+        this.form = {
+          id: "",
+          proof: [],
+        };
+      }
+    }
   },
   methods: {
-    handleFilter () {
+    handleFilter (value) {
+      console.log(1223233);
+
+      if (value == '选项1') {
+        this.listQuery.handle_status = '2'
+      }
+      else if (value == '选项2') {
+        this.listQuery.handle_status = '1'
+      }
       this.listQuery.page = 1;
       this.getList();
     },
@@ -143,20 +201,59 @@ export default {
     handleCurrentChange (val) {
       this.listQuery.page = val;
       this.getList();
-    }, getList () {
+    },
+    handle (row) {
+      this.dialogVisible = true;
+      this.form.id = row.id;
+      if (this.flag == 1) {
+        if (row.status == '1') {
+          row.status = '2'
+          this.flag = 0
+        }
+        else if (row.status == '2') {
+          row.status = '1'
+          this.flag = 0
+        }
+      }
+
+    },
+    getList () {
       this.listLoading = true;
+      console.log(123);
+
       request({
-        url: "/api/backend/finance/orderFlow",
+        url: "/api/backend/finance/orderCount",
         method: "get",
         params: this.listQuery
       }).then(response => {
+        console.log(response);
+
         this.list = response.data.data;
         this.total = response.data.total;
-        this.amount = response.data.order_amount;
         this.listLoading = false;
       });
 
     },
+    saveData () {
+      this.btnLoading = true;
+      request({
+        url: "/api/backend/finance/handle",
+        method: "post",
+        data: this.form
+      })
+        .then(() => {
+          this.flag = 1
+          this.$message({
+            type: "success",
+            message: "操作成功!"
+          });
+          this.dialogVisible = false;
+          this.getList()
+        })
+        .finally(() => {
+          this.btnLoading = false;
+        });
+    }
   }
 }
 </script>
